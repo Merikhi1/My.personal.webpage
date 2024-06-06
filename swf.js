ebd84afc2d589561441b16517d94061093262ac4
Merikhi1 <yassermerikhi@gmail.com>
2024-06-06 23:32:21 +0100

    Add files via upload

---
+++ swf.js
@@ -0,0 +1,9 @@
+function showSection(sectionId) {
+  // Hide all sections
+  document.querySelectorAll('.content-section').forEach(section => {
+    section.style.display = 'none';
+  });
+
+  // Show the selected section
+  document.getElementById(sectionId).style.display = 'block';
+}
